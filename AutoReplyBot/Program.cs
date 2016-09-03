c79fd1ad9c07ebc718bc93b2438ae6d637465ad6
121jigowatts <sh.yoshida@live.jp>
2016-09-04 01:20:16 +0900

    プロジェクト ファイルを追加します。

---
+++ AutoReplyBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using CoreTweet;
+using System;
+
+namespace AutoReplyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var APIKey = args[0];
+            var APISecret = args[1];
+            var AccessToken = args[2];
+            var AccessTokenSecret = args[3];
+            try
+            {
+                var t = Tokens.Create(APIKey
+                    , APISecret
+                    , AccessToken
+                    , AccessTokenSecret);
+                var replyer = new Replyer(t);
+                replyer.Observe();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+                throw;
+            }
+
+            Console.WriteLine($"[{DateTime.Now.ToString()}] 処理開始");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ AutoReplyBot/Replyer.cs
@@ -0,0 +1,65 @@
+﻿using CoreTweet;
+using CoreTweet.Streaming;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text.RegularExpressions;
+
+namespace AutoReplyBot
+{
+    public class Replyer
+    {
+        public Tokens Tokens { get; private set; }
+        public Replyer(Tokens tokens)
+        {
+            Tokens = tokens;
+        }
+
+        public void Observe()
+        {
+            var observable = Tokens.Streaming.UserAsObservable();
+
+            observable.Catch(
+                    observable.DelaySubscription(
+                        TimeSpan.FromSeconds(10)
+                        ).Retry()
+                )
+                .Repeat()
+                .Where((StreamingMessage m) => m.Type == MessageType.Create)
+                .Cast<StatusMessage>()
+                .Select((StatusMessage m) => m.Status)
+                .Subscribe(
+                    Next,
+                    (Exception ex) => Console.WriteLine(ex),
+                    () => Console.WriteLine("終点")
+                );
+        }
+        public void Next(Status status)
+        {
+            var createdAt = status.CreatedAt.LocalDateTime;
+            var screenName = status.User.ScreenName;
+            var text = status.Text;
+            var id = status.Id;
+
+            Console.WriteLine($"[{createdAt}] {screenName}: {text}");
+
+            var user = "USER_NAME";
+            var pattern = $@"@{user}\s.*Gigawatts.*";
+
+            if (Regex.IsMatch(text, pattern))
+            {
+                var reply = $@"@{screenName} Jigowatts!";
+
+                Console.WriteLine($"{reply}");
+                Tokens.Statuses.Update(
+                    new
+                    {
+                        status = reply,
+                        in_reply_to_status_id = id
+                    });
+
+            }
+        }
+
+    }
+}
